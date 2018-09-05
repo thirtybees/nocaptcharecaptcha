43a4db9be2abfdbcd2d3578ddb5ea7e807ad6fd1
Markus Hitter <mah@jump-ing.de>
2018-09-05 21:22:42 +0200

    Rename lib/ to libs/.
    This makes naming consistent with views/js/libs/ and
    views/css/libs/ and was also already implemented in this way in
    validatemodule.sh in thirty bees core.

--- nocaptcharecaptcha.php
+++ nocaptcharecaptcha.php
@@ -25,7 +25,7 @@ if (!defined('_TB_VERSION_')) {
     exit;
 }
 
-require_once dirname(__FILE__).'/lib/autoload.php';
+require_once dirname(__FILE__).'/libs/autoload.php';
 require_once dirname(__FILE__).'/classes/autoload.php';
 
 /**
