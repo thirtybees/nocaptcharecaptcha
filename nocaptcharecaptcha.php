7dcc863a719097e480c5a3ed85209eff108008bf
Petr Hučík <petr@getdatakick.com>
2019-11-27 10:45:31 +0100

    version 1.1.1

--- nocaptcharecaptcha.php
+++ nocaptcharecaptcha.php
@@ -99,7 +99,7 @@ class NoCaptchaRecaptcha extends Module
     {
         $this->name = 'nocaptcharecaptcha';
         $this->tab = 'front_office_features';
-        $this->version = '1.1.0';
+        $this->version = '1.1.1';
         $this->author = 'thirty bees';
         $this->need_instance = 1;
 
