628337417a2c6bd198c3468cb49a85e4201ef617
therampagerado <office@rampagesport.eu>
2024-05-07 17:41:33 +0300

    Update years and use HTTPS for links (where needed) in license headers.
    Update years and use HTTPS for links (where needed) in license headers.

--- classes/RecaptchaLib.php
+++ classes/RecaptchaLib.php
@@ -1,26 +1,26 @@
 <?php
 /**
- * Copyright (C) 2017-2018 thirty bees
+ * Copyright (C) 2017-2024 thirty bees
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Academic Free License (AFL 3.0)
  * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
+ * https://opensource.org/licenses/afl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@thirtybees.com so we can send you a copy immediately.
  *
  * @author    thirty bees <modules@thirtybees.com>
- * @copyright 2017-2018 thirty bees
+ * @copyright 2017-2024 thirty bees
  * @license   Academic Free License (AFL 3.0)
  */
 
 /**
  * This is a PHP library that handles calling reCAPTCHA.
  *
- * @link      http://www.google.com/recaptcha
+ * @link      https://www.google.com/recaptcha
  * @author    Google, Inc.
  * @copyright Copyright (c) 2014, Google Inc.
  * @license   public domain
@@ -48,7 +48,7 @@
  * Get a reCAPTCHA API Key
  * https://www.google.com/recaptcha/admin/create
  * Discussion group
- * http://groups.google.com/group/recaptcha
+ * https://groups.google.com/group/recaptcha
  */
 
 namespace NoCaptchaRecaptchaModule;
